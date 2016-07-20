a863c8bc702e887ae1bd75a67ab66ec0f66c46c6
caiwb <whenbber@gmail.com>
2016-07-21 04:24:33 +0800

    bugfix

--- Example/BCWechatShareActivity/BCAppDelegate.m
+++ Example/BCWechatShareActivity/BCAppDelegate.m
@@ -11,12 +11,9 @@
 
 @implementation BCAppDelegate
 
-static const NSString *WXAppId;
-static const NSString *WXAppSecret;
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(nullable NSDictionary *)launchOptions NS_AVAILABLE_IOS(3_0)
 {
-    [[BCWXSocialHandler sharedInstance] setWXAppId:WXAppId appSecret:WXAppSecret];
+    return [[BCWXSocialHandler sharedInstance] setWXAppId:@"appId" appSecret:@"appKey"];
 }
 
 
