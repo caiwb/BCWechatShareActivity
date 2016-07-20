6bba43a21218e06bb1072517f0a9fa317e437474
caiwenbo <caiwenbo@rd.netease.com>
2016-07-20 23:50:04 +0800

    add example

---
+++ BCWechatShareActivity/Classes/BCWXShareActivity.h
@@ -0,0 +1,18 @@
+//
+//  BCWXShareActivity.h
+//  Pods
+//
+//  Created by caiwb on 16/7/20.
+//
+//
+
+#import "BCWXBaseActivity.h"
+#import "BCWXSessionActivity.h"
+#import "BCWXTimelineActivity.h"
+
+#import "BCWXLoginProvider.h"
+#import "BCWXSocialHandler.h"
+
+#import "WXApi.h"
+#import "WXApiObject.h"
+
--- Example/BCWechatShareActivity/BCViewController.m
+++ Example/BCWechatShareActivity/BCViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BCViewController.h"
+#import "BCWXShareActivity.h"
 
 @interface BCViewController ()
 
@@ -14,16 +15,54 @@
 
 @implementation BCViewController
 
+- (void)loginAndGetUserInfo
+{
+    [BCWXLoginProvider loginWithCompleteBlock:^(BOOL suc, NSString *accessToken, NSString *openId, NSString *errMsg) {
+        if (suc)
+        {
+            [BCWXLoginProvider getUserInfoWithCompleteBlock:^(BOOL suc, BCWXUserInfo *userInfo) {
+                if (suc)
+                {
+                    //use userInfo
+                }
+            }];
+        }
+    }];
+}
+
+- (void)share
+{
+    [BCWXShareProvider shareWebPage:BCWX_SESSION
+                          withTitle:@"分享到微信"
+                               text:@"内容"
+                              image:[UIImage imageNamed:@"thumbImage"]
+                             webUrl:@"github.com"
+                           complete:^(BOOL suc, NSString *errMsg) {
+                               //to do after share
+                           }];
+}
+
+- (void)shareOnActivityController
+{
+    BCWXSessionActivity *item = [[BCWXSessionActivity alloc] init];
+    
+    item.title = @"title";
+    item.text = @"content";
+    item.thumbImage = [UIImage new];
+    item.webUrl = @"youdao.com";
+    [item setCompleteBlock:^(BOOL suc, NSString *errMsg) {
+        //to do after share
+    }];
+    
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[item.title, item.webUrl, item.thumbImage] applicationActivities:@[item]];
+    [self presentViewController:activityVC animated:YES completion:nil];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self loginAndGetUserInfo];
 }
 
 @end
